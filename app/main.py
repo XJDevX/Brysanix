a8a2f59482603a51dcf089db21829a83788d2af0
JXDev <julianparrales19@gmail.com>
2024-12-28 07:33:16 -0500

    Analysis 1.0

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 #<<---Creating the Main IA-Assistant--->>#
 import pandas as pd
+import os
 import prod_revenue
 
 def convert_data(csv):
-    pd.read_csv(csv)
+    if os.path.exists(csv):
+        return pd.read_csv(csv)
+    else:
+        print(f"Error: The file {csv} does not exist")
+        return None
 
 def run():
-    convert_data('./data/prod_data.csv')
-    prod_revenue.revenue()
+    data = convert_data('./data/prod_data.csv')
+    print(f"Data: \n{data}")
+    # prod_revenue.revenue()
 
 if __name__ == "__main__":
     run()
